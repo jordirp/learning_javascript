a59103bb8e160aa824eec18b362777bc328f1fbb
jordirp <jordirodriguez2@iesebre.com>
2021-11-22 17:46:09 +0100

    WIP

---
+++ vue/counter.vue
@@ -0,0 +1,24 @@
+<Template>
+  <div>
+    <p>{{counter}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+  </div>
+</Template>
+<script>
+
+export default  {
+
+    data() {
+      return{counter: 0}
+    },
+    methods: {
+      increment() {
+        this.counter++
+      },
+      decrement() {
+        this.counter--
+      },
+    }
+  }
+</script>
