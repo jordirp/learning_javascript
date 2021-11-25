db3a211c0cc26bac7548c70fc29284d6e5cbe7c0
jordirp <jordirodriguez2@iesebre.com>
2021-11-25 15:59:15 +0100

    Versio amb VueCli comptador (fet)

---
+++ comptador_vuecli/src/components/counter.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <p>{{ counter }}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement()">-</button>
+  </div>
+</template>
+
+<script>
+
+export default  {
+
+    data() {
+      return{counter: 0}
+    },
+    methods: {
+      increment() {
+        this.counter++
+      },
+      decrement() {
+        this.counter--
+      },
+    }
+  }
+</script>
+<styles>
+
+</styles>
